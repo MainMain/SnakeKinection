f723034756458868836ea7c7fe4a84e2f2b85c43
MainMain <j.brun2@live.fr>
2014-10-16 13:37:40 +0200

    Dépôt des sources avec modélisation du terrain, scripts OK

---
+++ Assets/Game Snake/scripts/MvtClavierScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public int speed = 10;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		float mouveHorizontal = Input.GetAxis("Horizontal");
+		float mouveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 mouvment = new Vector3(mouveHorizontal, 0, mouveVertical);
+		rigidbody.AddForce(mouvment * speed * Time.deltaTime);
+		
+	}
+}
---
+++ Assets/Game Snake/scripts/OnClickMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Clic sur un bouton du menu
+public class OnClickMenu : MonoBehaviour {
+
+	TextMesh[] texts;
+	public AudioClip sound;
+
+	//Define Enum
+	public enum MenuOptions {Jouer, Options, Appliquer, Quitter};
+	//This is what you need to show in the inspector.
+	public MenuOptions OptionMenuSelected;
+
+
+	void Start(){
+		texts = GetComponentsInChildren<TextMesh>();
+	}
+	
+	void OnMouseEnter(){
+		foreach (TextMesh child in texts)
+		{
+			child.color = Color.red;
+		}
+	}
+	
+	void OnMouseExit(){
+		foreach (TextMesh child in texts)
+		{
+			child.color = Color.white;
+		}
+	}
+	
+	void OnMouseDown() {
+		if (OptionMenuSelected.Equals(MenuOptions.Jouer))
+		{
+			Application.LoadLevel("snakescene"); 
+		}
+		if (OptionMenuSelected.Equals(MenuOptions.Options))
+		{
+			Application.LoadLevel("OptionSnake"); 
+		}
+		if (OptionMenuSelected.Equals(MenuOptions.Appliquer))
+		{
+			Application.LoadLevel("MenuSnake"); 
+		}
+		if (OptionMenuSelected.Equals(MenuOptions.Quitter))
+		{
+			Application.LoadLevel(0); 
+		}
+		AudioSource.PlayClipAtPoint(sound, transform.position);
+	}
+}
